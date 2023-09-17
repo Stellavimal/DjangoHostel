211e2d71860e564ca8385746e6b0048091591592
jency <riyanajency123@gmail.com>
2023-09-17 15:04:05 +0530

    changes

--- hostelproject/hostelapp/models.py
+++ hostelproject/hostelapp/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-
+import datetime
+ROOMTYPE_CHOICE=[
+   ('Single bed','Single bed'),
+   ('Double bed','Double bed'),
+   ('More beds','More beds'),
+ ]
 # Create your models here.
 class rooms(models.Model):
+    Room_id=models.IntegerField(null=True,blank=True)
     name=models.CharField(max_length=20)
     checkin=models.DateField()
     checkout=models.DateField()
-    Roomtype=models.CharField(max_length=30)
+    Roomtype=models.CharField(max_length=30,choices=ROOMTYPE_CHOICE)
+
 
     class Meta:
        verbose_name_plural="rooms"
